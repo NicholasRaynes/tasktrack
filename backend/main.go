5de9dbdd21d8af08e58638c2119bb4f5f135262e
NicholasRaynes <nicholasraynesmb@gmail.com>
2025-05-28 20:20:46 -0500

    Set up server in main.go

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"tasktrack/backend/config"
+	"tasktrack/backend/database"
+	"tasktrack/backend/routes"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-    fmt.Println("TaskTrack backend is running!")
+	config.LoadEnv()
+	database.Connect()
+
+	r := gin.Default()
+	routes.RegisterRoutes(r)
+
+	port := os.Getenv("PORT")
+	r.Run(":" + port)
 }
