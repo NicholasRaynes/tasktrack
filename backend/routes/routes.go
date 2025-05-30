4f3497f869c8a1d332596d13e1cf61a111865194
NicholasRaynes <nicholasraynesmb@gmail.com>
2025-05-29 19:56:16 -0500

    Protected routes

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -2,10 +2,19 @@ package routes
 
 import (
 	"github.com/NicholasRaynes/tasktrack/backend/controllers"
+	"github.com/NicholasRaynes/tasktrack/backend/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterRoutes(r *gin.Engine) {
 	r.GET("/health", controllers.HealthCheck)
+	r.POST("/signup", controllers.Signup)
+	r.POST("/login", controllers.Login)
+
+	protected := r.Group("/api")
+	protected.Use(middleware.AuthMiddleware())
+	protected.GET("/profile", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "You are authenticated 🎉"})
+	})
 }
